7f86c59da7a983a3cfe1b7ea87a016ef41b6f332
Julian Montes Ramirez <damon.198623@gmail.com>
2025-08-18 18:46:35 +0000

    cambia el tamaño y pon 240x280 pixeles en /login

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image';
 export function GemelliFixLogo(props: { className?: string }) {
   return (
     <Image 
-      src="https://placehold.co/240x48.png" 
+      src="https://placehold.co/240x280.png" 
       alt="GemelliFix Logo Placeholder" 
       width={240} 
-      height={48} 
+      height={280} 
       className={props.className}
       priority
       data-ai-hint="logo"
