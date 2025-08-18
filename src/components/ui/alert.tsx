9b450bd3b58e75c81e23eca0d209df45e8e5485b
Julian Montes Ramirez <damon.198623@gmail.com>
2025-08-18 17:22:30 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -22,7 +22,7 @@ const alertVariants = cva(
 const Alert = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
->(({ className, ...props }, ref) => (
+>(({ className, variant, ...props }, ref) => (
   <div
     ref={ref}
     role="alert"
