4bffa82100e42a8a19cbb917d68e4de86cb8b4b6
Julian Montes Ramirez <damon.198623@gmail.com>
2025-08-18 18:57:00 +0000

    https://firebasestorage.googleapis.com/v0/b/gemellifix.firebasestorage.a

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,13 +1,17 @@
 
 import type { SVGProps } from 'react';
+import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 export function GemelliFixLogo(props: { className?: string }) {
   return (
-    <img 
-      src="/logo.png" 
+    <Image 
+      src="https://firebasestorage.googleapis.com/v0/b/gemellifix.firebasestorage.app/o/Logo.png?alt=media&token=3c91d664-c1d3-43b0-b81f-2b21a7cf2c05" 
       alt="GemelliFix Logo" 
-      className={cn("h-auto", props.className)}
+      width={1024} 
+      height={256} 
+      className={cn("h-auto w-full", props.className)}
+      priority
       data-ai-hint="logo"
     />
   );
