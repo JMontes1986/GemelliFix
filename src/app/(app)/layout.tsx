9824732be5f3eb2599933a1d78bdf1d88fa1ef6e
Julian Montes Ramirez <damon.198623@gmail.com>
2025-08-19 01:00:37 +0000

    necesito que crees el botón de ticket en /tickets/create, este botón lo

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -234,6 +234,25 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
         <main className="flex-1 p-4 sm:px-6 sm:py-0 space-y-4">
           {children}
         </main>
+         {showFab && (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link href="/tickets/create">
+                  <Button
+                    className="fixed bottom-6 right-6 h-16 w-16 rounded-full shadow-lg z-50"
+                  >
+                    <PlusCircle className="h-8 w-8" />
+                    <span className="sr-only">Crear Nuevo Ticket</span>
+                  </Button>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="left">
+                Crear Nuevo Ticket
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
       </SidebarInset>
     </SidebarProvider>
   );
