b56f4c7d11ac8e251633074b48b4a8fdeecc0b83
Julian Montes Ramirez <damon.198623@gmail.com>
2025-08-21 20:42:04 +0000

    yo ya lo tengo pero en que parte te lo pongo en la carpeta public para q

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
