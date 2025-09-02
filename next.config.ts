4eeb7be0aef145c719dd167817f23a61acbf74da
Julian Montes Ramirez <damon.198623@gmail.com>
2025-09-02 14:27:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const securityHeaders = [
       "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://securetoken.googleapis.com https://www.googleapis.com https://firebasestorage.googleapis.com",
       "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.gstatic.com https://www.googletagmanager.com https://www.google.com https://apis.google.com blob:",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-      "img-src 'self' data: blob: https: https://lh3.googleusercontent.com https://*.googleusercontent.com https://firebasestorage.googleapis.com",
+      "img-src 'self' data: blob: https: https://lh3.googleusercontent.com https://*.googleusercontent.com https://firebasestorage.googleapis.com https://picsum.photos",
       "font-src 'self' https://fonts.gstatic.com data:",
       "media-src 'self' blob: data:",
     ].join('; ')
