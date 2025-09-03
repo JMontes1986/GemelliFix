a8fc698514a18a3133e30d339e4666025eb2b1af
JMontes <damon.1986@icloud.com>
2025-09-02 19:04:02 -0500

    supabase_client.py

--- python/supabase_client.py
+++ python/supabase_client.py
@@ -2,9 +2,13 @@ from supabase import create_client, Client
 import os
 
 url = os.environ.get('SUPABASE_URL') or ''
+
+# Use the service role key when available for privileged operations.
+# This key should only be set in secure server-side environments.
+service_key = os.environ.get('SUPABASE_SERVICE_ROLE_KEY') or ''
 anon_key = os.environ.get('SUPABASE_ANON_KEY') or ''
 
-supabase: Client = create_client(url, anon_key)
+supabase: Client = create_client(url, service_key or anon_key)
 
 
 def fetch_profiles():
